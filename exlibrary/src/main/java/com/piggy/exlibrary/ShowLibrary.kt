e77ae2cb21f8dc39cebda61a16ea3eaddd5329f4
mingu <mingu@duse.co.kr>
2022-07-31 18:45:35 +0900

    commit

---
+++ exlibrary/src/main/java/com/piggy/exlibrary/ShowLibrary.kt
@@ -0,0 +1,15 @@
+package com.piggy.exlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ShowLibrary {
+    companion object {
+
+        fun sToast(context: Context, msg: String) =
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+
+        fun lToast(context: Context, msg: String) =
+            Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
+    }
+}
